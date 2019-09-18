f66c5f81eada39f47507cc9c23977fe859e1e639
bannzai <kingkong999yhirose@gmail.com>
2019-09-18 13:20:39 +0900

    :sparkles: Add demo file

---
+++ UserDefaultsGeneratorDemo/UserDefaultsGeneratorDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserDefaultsGeneratorDemo
+//
+//  Created by Yudai Hirose on 2019/09/18.
+//  Copyright © 2019 Yudai Hirose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
