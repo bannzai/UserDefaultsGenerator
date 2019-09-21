d7336c4eea8247f714cefd7ed1a0788cdcbf98db
bannzai <kingkong999yhirose@gmail.com>
2019-09-21 18:39:31 +0900

    :recycle: fix indent and missing condition

--- Sources/UserDefaultsGenerator/main.swift
+++ Sources/UserDefaultsGenerator/main.swift
@@ -1,29 +1,30 @@
 import Foundation
 import UserDefaultsGeneratorCore
 
-let arguments = ProcessInfo.processInfo.arguments
-if arguments.count <= 1, arguments[1] != "help" {
-    print("""
-        Missing argument.
-        See below
-        \(HelpRunner.help())
-""")
-    exit(1)
-}
-let subCommand = ProcessInfo.processInfo.arguments[1]
-
 let runners: [Runner.Type] = [
     GenerateRunner.self,
     SetupRunner.self,
     HelpRunner.self
 ]
 
+let arguments = ProcessInfo.processInfo.arguments
+if arguments.count <= 1 || !runners.contains { $0.commandName == arguments[1] } {
+    print("""
+Missing argument.
+See below
+\(HelpRunner.help())
+""")
+    exit(1)
+}
+let subCommand = ProcessInfo.processInfo.arguments[1]
+
+
 guard let command = runners.first(where: { $0.commandName == subCommand }) else {
     print("""
-        Unexpected sub command \(subCommand)
-        See below
-        \(HelpRunner.help())
-        """)
+Unexpected sub command \(subCommand)
+See below
+\(HelpRunner.help())
+""")
     exit(1)
 }
 
